eb4988a2aecbf86f0dacc130ea7675ca6032343f
Andhika Gilang Rahadian <rahadian.kidz92@gmail.com>
2025-09-03 22:33:05 +0700

    Update all changes: UI, assets, packages, and other files

--- lib/onboarding_one/city_three.dart
+++ lib/onboarding_one/city_three.dart
@@ -8,7 +8,40 @@ class CityThree extends StatelessWidget {
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
-        children: [Image.asset("assets/city-3.png", fit: BoxFit.cover)],
+        children: [
+          Image.asset("assets/city-3.png", fit: BoxFit.cover),
+          Positioned(
+            bottom: 40,
+            left: 0,
+            right: 0,
+            child: Padding(
+              padding: const EdgeInsets.all(40),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Your Title Goes \nHere",
+                    style: TextStyle(
+                      height: 1.25,
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta ipsum",
+                    style: TextStyle(
+                      height: 1.25,
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
